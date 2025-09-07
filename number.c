54abbc82a87853fb2aa20794f59fb67cdd00d6dc
Sokhna Diokhane <smd10055@nyu.edu>
2025-09-07 20:15:54 +0400

    finish assignment

--- number.c
+++ number.c
@@ -5,5 +5,15 @@ int main() {
   printf("Enter a number: ");
   scanf("%d", &number);
 
-  if (number)
+  int readnum = number;
+  if (readnum % 3 == 0 && readnum % 5 == 0) {
+    printf("%s \n", "foobar");
+  } else if (readnum % 3 == 0) {
+    printf("%s \n", "foo");
+  } else if (readnum % 5 == 0) {
+    printf("%s \n", "bar");
+  } else {
+    printf("%s\n", "Entry not valid try again! ");
+  }
+  return 0;
 }
